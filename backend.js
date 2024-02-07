0ed91f96b689f2df71bd0796f3571f85e3ecd1c2
Gaurang Tyagi <gaurangtyagi317@gmail.com>
2024-02-08 00:07:53 +0530

    initial

---
+++ backend.js
@@ -0,0 +1,77 @@
+
+import express from "express";
+import fs from "fs"
+import cors from "cors"
+import nodemailer from "nodemailer"
+import cron from "node-cron"
+
+const app = express()
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
+
+let flag = false;
+const notifyUser = async () => {
+    const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port : 465,
+        secure: true,
+        auth: {
+            user: "gaurangtyagi7@gmail.com",
+            pass : "rqhhxyfravzryhtb"
+        }
+    })
+    const info = await transporter.sendMail({
+        from: "gaurangtyagi7@gmail.com",
+        to: "gaurangtyagi7@gmail.com",
+        subject: "Reminder",
+        html : "<h1>Need to buy medicines<h1>"
+    })
+    return info;
+}
+
+app.get("/", (req, res) => {
+    let data = fs.readFileSync("shops.json",{encoding:"utf-8"})
+    data = JSON.parse(data)
+    let temp = Array()
+    data.forEach(ele => {
+        temp.push(ele.Medicines)
+    })
+    res.header({'content-type':"application/json"})
+    res.status(200).json(temp)
+})
+app.post("/getList", (req, res) => {
+    let { d,days } = req.body
+    d= d.split(",")
+    const calc = (ammountLeft) => {
+        let data = fs.readFileSync("shops.json",{encoding:"utf-8"})
+        data = JSON.parse(data)
+        let daysLeft = []
+        data.forEach((shops) => {
+            const {Medicines} = shops
+            Medicines.forEach((med, index) => {
+                const {perPack,perDay,Price} = med
+                let left = (Math.round(days - (parseInt(ammountLeft[index]) / perDay)))
+                let needed = left * perDay
+                needed = Math.ceil(needed / perPack)
+
+                let name = Object.keys(med)[0]
+                daysLeft.push({name,needed,price:needed*parseInt(Price)})
+            })
+        })
+        flag = true;
+        return daysLeft
+    }
+    const result = calc(d)
+    res.json(result)
+})
+
+app.listen(5000);
+
+cron.schedule("0 0 */28 * *", () => {
+    if (flag) {
+        let msg = notifyUser();
+        console.log(msg.messageId)
+        flag = false;
+    }
+    
+})
---
+++ index.js
@@ -0,0 +1,59 @@
+console.log("hello")
+const realDate = $("#real_date")
+const btn1 = $("#btn-1");
+const date = new Date()
+
+
+$(realDate).text(date.toDateString())
+
+btn1.on("click", e => {
+    $("#hid")[0].setAttribute("data-hidden", false)
+    getData()
+})
+const getData = async () => {
+    let response = await fetch("http://localhost:5000", { method: "GET" });
+    response = await response.json()
+    let table = $("#table")
+    response.forEach(shop => {
+        shop.forEach((medicine, index) => {
+            let cost = Object.values(medicine)[0]
+            let key = Object.keys(medicine)[0]
+
+            let tr = document.createElement("tr")
+            tr.innerHTML += `<td>${key}</td>\n
+            <td>Rs.${cost}</td>\n
+            <td><input type='number' class='amount' value=6></td>
+            `
+            table.append(tr)
+        })
+    })
+    const days = $("#days")
+    days.on("change", e => {
+        $("#btn3")[0].innerText = `Calculate for next ${days.val()} days`
+    })
+    $("#btn3").on("click", async e => {
+        $("#hid1")[0].setAttribute("data-hidden", false)
+        let data = []
+        $(".amount").each((index, ele) => {
+            data.push(ele.value)
+        })
+        let newData = new URLSearchParams({ d: data, days: days.val() })
+        let response = await fetch("http://localhost:5000/getList", { method: "POST", body: newData })
+        response = await response.json()
+        let sum = 0;
+        const tab = $("#res")[0]
+        response.forEach(ele => {
+            let tr = document.createElement("tr")
+            tr.innerHTML = `<td>${ele.name}</td>\n
+            <td>${ele.needed}</td>\n
+            <td>Rs.${ele.price}</td>\n
+            `
+            tab.append(tr)
+            sum += ele.price
+        })
+        const d = new Date()
+        d.setDate(d.getDate() + 30)
+        $("#cost").text(`Total cost Rs. ${sum}`)
+        $("#last").text(`Last Date: ${d.toDateString()}`)
+    })
+}
