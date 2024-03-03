bf87a57e8deb4e118e373406622997c4794185ef
Gaurang Tyagi <gaurangtyagi317@gmail.com>
2024-03-03 11:19:37 +0530

    minor change

--- backend.js
+++ backend.js
@@ -67,7 +67,7 @@ app.post("/getList", (req, res) => {
 
 app.listen(5000);
 
-cron.schedule("0 0 */24 * *", () => {
+cron.schedule("0 0 */1 * *", () => {
     if (flag) {
         let msg = notifyUser();
         console.log(msg.messageId)
